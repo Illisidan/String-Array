c8ed93b675828cf4c165424a4f72640b6cc29d4c
Illisidan <gi.jon.531@gmail.com>
2018-08-02 23:57:16 +0800

    Working on getting the code to work together before I move on

--- Grades/src/Grade/Main.java
+++ Grades/src/Grade/Main.java
@@ -3,12 +3,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] subjects = {"[English, ", "Chinese, ", "Math, ", "Science, ", "History]"};
-        /*for(String subject: subjects) {
+        /*String[] subjects = {"[English, ", "Chinese, ", "Math, ", "Science, ", "History]"};
+        for(String subject: subjects) {
             System.out.print(subject);
-        }*/
+        }*/ //Can't get this to work
+
+        System.out.println("English, Math, Chinese, Science, History.");
             Scanner input = new Scanner(System.in);
             System.out.println("Input grade score for English: ");
-            int grade = input.nextInt();
+            int eng = input.nextInt();
+            System.out.println("Input grade score for Maths: ");
+            int math = input.nextInt();
+            System.out.println("Input grade for Chinese: ");
+            int chn = input.nextInt();
+            System.out.println("Input grade for Science: ");
+            int sci = input.nextInt();
+            System.out.println("Input grade for History: ");
+            int his = input.nextInt();
+
+            if (eng < 50 ) {
+                System.out.println("English: F");
+            }
+                else if (eng < 60 ) {
+                    System.out.println("English: D.");
+                }
+                else if (eng < 70 ) {
+                    System.out.println("English: C");
+                }
+                else if (eng < 80 ) {
+                    System.out.println("English: B");
+                        }
+                else if (eng < 90 ) {
+                    System.out.println("English: A");
+                        }
+                        else {
+                System.out.println("Invalid score");
+            }
+
+
+
     }
 }
