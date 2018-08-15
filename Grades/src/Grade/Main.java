fae838e451a96a961d57bf7576b1df6f3d9b1b76
Illisidan <gi.jon.531@gmail.com>
2018-08-15 23:58:22 +0800

    Am I on the right path?

--- Grades/src/Grade/Main.java
+++ Grades/src/Grade/Main.java
@@ -3,44 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*String[] subjects = {"[English, ", "Chinese, ", "Math, ", "Science, ", "History]"};
+        String[] subjects = {"[English, ", "Chinese, ", "Math, ", "Science, ", "History]\n"};
         for(String subject: subjects) {
             System.out.print(subject);
-        }*/ //Can't get this to work
-
-        System.out.println("English, Math, Chinese, Science, History.");
-            Scanner input = new Scanner(System.in);
-            System.out.println("Input grade score for English: ");
-            int eng = input.nextInt();
-            System.out.println("Input grade score for Maths: ");
-            int math = input.nextInt();
-            System.out.println("Input grade for Chinese: ");
-            int chn = input.nextInt();
-            System.out.println("Input grade for Science: ");
-            int sci = input.nextInt();
-            System.out.println("Input grade for History: ");
-            int his = input.nextInt();
-
-            if (eng < 50 ) {
-                System.out.println("English: F");
-            }
-                else if (eng < 60 ) {
-                    System.out.println("English: D.");
-                }
-                else if (eng < 70 ) {
-                    System.out.println("English: C");
-                }
-                else if (eng < 80 ) {
-                    System.out.println("English: B");
-                        }
-                else if (eng < 90 ) {
-                    System.out.println("English: A");
-                        }
-                        else {
-                System.out.println("Invalid score");
-            }
-
-
-
+        }
+    int grade;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input grade for English: \n");
+        grade = input.nextInt();
+        if (grade < 60 ) {
+            System.out.print("D");
+        }
     }
 }
